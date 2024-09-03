d6b9710ef5d424af23526f837ee553b978fb9647
chspchal <122350485+chspchal@users.noreply.github.com>
2024-09-03 17:02:38 +0700

    Add files via upload

---
+++ lib/component/profile_menu.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:mycool_profile/constant/constant.dart';
+
+class ProfileMenu extends StatelessWidget {
+  final String title;
+  final IconData icons;
+
+  const ProfileMenu({
+    Key? key,
+    required this.title,
+    required this.icons,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width:280,
+      height: 60,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,      
+              children: [
+                CircleAvatar(
+                  backgroundColor: bgThirdColor,
+                  radius: 20,
+                  child: Icon(
+                    icons, 
+                    size:24, 
+                    color: iconPrimaryColor),
+                ),
+                const SizedBox(width: 10),
+                Text(title, style: textMenu,),
+                const Spacer(),
+                const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 16,
+                  child: const Icon(
+                    Icons.arrow_forward_ios, 
+                    size:20, 
+                    color: iconSecondaryColor),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
